ab13eb7995ccc8145ccfe9402960d036fa2ce843
Fahd Mannaa <fahdkuwait@gmail.com>
2024-05-07 08:45:23 +0300

    sync

--- rag_backend.py
+++ rag_backend.py
@@ -7,16 +7,17 @@ from langchain.vectorstores import FAISS
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms.bedrock import Bedrock
 
-#5c. Wrap within a function
+#5c. Load the data, embed and stor in vector db
 def hr_index():
     #2. Define the data source and load data with PDFLoader(https://www.upl-ltd.com/images/people/downloads/Leave-Policy-India.pdf)
-    data_load=PyPDFLoader('https://www.upl-ltd.com/images/people/downloads/Leave-Policy-India.pdf')
+    #data_load=PyPDFLoader('https://www.upl-ltd.com/images/people/downloads/Leave-Policy-India.pdf')
+    data_load=PyPDFLoader('https://hr-public.s3.eu-west-1.amazonaws.com/HR+Policies+and+Procedures+Manual+-+BTQ+-+KW+-+SOP+-+001.01.pdf')
     #3. Split the Text based on Character, Tokens etc. - Recursively split by character - ["\n\n", "\n", " ", ""]
     data_split=RecursiveCharacterTextSplitter(separators=["\n\n", "\n", " ", ""], chunk_size=100,chunk_overlap=10)
     #4. Create Embeddings -- Client connection
     data_embeddings=BedrockEmbeddings(
-    credentials_profile_name= 'default',
-    model_id='amazon.titan-embed-text-v1')
+        credentials_profile_name= 'default',
+        model_id='amazon.titan-embed-text-v1')
     #5à Create Vector DB, Store Embeddings and Index for Search - VectorstoreIndexCreator
     data_index=VectorstoreIndexCreator(
         text_splitter=data_split,
@@ -25,6 +26,7 @@ def hr_index():
     #5b  Create index for HR Policy Document
     db_index=data_index.from_loaders([data_load])
     return db_index
+
 #6a. Write a function to connect to Bedrock Foundation Model - Claude Foundation Model
 def hr_llm():
     llm=Bedrock(
@@ -35,6 +37,7 @@ def hr_llm():
         "temperature": 0.1,
         "top_p": 0.9})
     return llm
+
 #6b. Write a function which searches the user prompt, searches the best match from Vector DB and sends both to LLM.
 def hr_rag_response(index,question):
     rag_llm=hr_llm()
